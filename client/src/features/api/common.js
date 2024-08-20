0dc287cafa9e84eb529e1fbadb69c303ce4483a5
muhzinmuhammed <muhzinsidhiq333@gmail.com>
2024-08-20 23:21:27 +0530

    change backend url

--- client/src/features/api/common.js
+++ client/src/features/api/common.js
@@ -1,7 +1,7 @@
 export const all={
     // baseurl:"http://192.168.1.225:8000/api"
     // baseurl:"http://localhost:8000/api",
-    baseurl:"http://localhost:8080/api",//mozhin
+    baseurl:"https://qurinom-backend-evjl.onrender.com/api",//mozhin
     
     
 }
